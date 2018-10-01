5891c5d7d2a64e778f2ca97f5a16b8e42af953b6
Futturu Comunicação <agencia@futturu.com.br>
2018-10-01 12:35:03 -0300

    Update ffmpeg-install.sh

--- ffmpeg-install.sh
+++ ffmpeg-install.sh
@@ -1,3 +1,5 @@
+This next step is optional but I recommend it. Install handbreak and compress a “web optimized” mp4 in whatever resolution you want
+
 # Installation
 brew install ffmpeg --with-vpx --with-vorbis --with-libvorbis --with-vpx --with-vorbis --with-theora --with-libogg --with-libvorbis --with-gpl --with-version3 --with-nonfree --with-postproc --with-libaacplus --with-libass --with-libcelt --with-libfaac --with-libfdk-aac --with-libfreetype --with-libmp3lame --with-libopencore-amrnb --with-libopencore-amrwb --with-libopenjpeg --with-openssl --with-libopus --with-libschroedinger --with-libspeex --with-libtheora --with-libvo-aacenc --with-libvorbis --with-libvpx --with-libx264 --with-libxvid
 
